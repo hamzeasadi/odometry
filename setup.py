c8eb4025d095933e794af1ea89d8b0c6ef96d4f5
Hamzeh Asadi <hamzeasadi1@gmail.com>
2023-11-05 22:14:25 +0000

    fixed the issue with setup file

--- setup.py
+++ setup.py
@@ -11,8 +11,11 @@ from setuptools import find_packages
 NAME = "odometry"
 DESCRIPTION = "a library for video liveness verification based on visual odometry"
 VERSION = "0.1.0"
+LONG_DESCRIPTION = ""
 with open("README.md", "r", encoding="utf-8") as readme:
-    LONG_DESCRIPTION = readme.readlines()
+    lines = readme.readlines()
+    for line in lines:
+        LONG_DESCRIPTION += line
 
 AUTHOR = "Hamzeh Asadi"
 LICENSE = "MIT"
